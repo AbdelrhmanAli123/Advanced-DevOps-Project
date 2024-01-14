dae00a90544b9a3ae464e071ecbcc38fe7913e49
Abdelrhman <abdelrhmanali@gmail.com>
2024-01-14 06:51:08 +0200

    create ECR EKS RDS SG and Network modules

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "Network" {
+  source           = "/home/ballo/terra/network"
+  vpc_cidr         = var.vpc_cidr
+  vpc_tag_name     = var.vpc_tag_name
+  EKS_subnets_cidr = var.EKS_subnets_cidr
+  RDS_subnets_cidr = var.RDS_subnets_cidr
+  az               = var.az
+}
+
+module "SG" {
+  source           = "/home/ballo/terra/SG"
+  vpc_id           = module.Network.vpc_id
+  sg-values        = var.sg-values
+}
+
+module "EKS" {
+  source           = "/home/ballo/terra/EKS"
+  cluster_name     = var.cluster_name
+  EKS_subnets_id   = module.Network.EKS_subnets_id
+  sg_id            = module.SG.sg_id
+  nodes_configs    = var.nodes_configs
+}
+
+module "ECR" {
+  source           = "/home/ballo/terra/ECR"
+  ecr_info         = var.ecr_info
+}
+
+module "RDS" {
+  source           = "/home/ballo/terra/RDS"
+  RDS_subnets_id   = module.Network.RDS_subnets_id
+  sg_id            = module.SG.sg_id
+  rds_info         = var.rds_info
+}
