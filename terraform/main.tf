6587dd631fd2d620b27445182d2ce3080eae2072
Abdelrhman <abdelrhmanali@gmail.com>
2024-01-14 07:00:12 +0200

    update the path of the modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 module "Network" {
-  source           = "/home/ballo/terra/network"
+  source           = "/home/ballo/terra/terraform/network"
   vpc_cidr         = var.vpc_cidr
   vpc_tag_name     = var.vpc_tag_name
   EKS_subnets_cidr = var.EKS_subnets_cidr
@@ -21,13 +21,13 @@ module "Network" {
 }
 
 module "SG" {
-  source           = "/home/ballo/terra/SG"
+  source           = "/home/ballo/terra/terraform/SG"
   vpc_id           = module.Network.vpc_id
   sg-values        = var.sg-values
 }
 
 module "EKS" {
-  source           = "/home/ballo/terra/EKS"
+  source           = "/home/ballo/terra/terraform/EKS"
   cluster_name     = var.cluster_name
   EKS_subnets_id   = module.Network.EKS_subnets_id
   sg_id            = module.SG.sg_id
@@ -35,12 +35,12 @@ module "EKS" {
 }
 
 module "ECR" {
-  source           = "/home/ballo/terra/ECR"
+  source           = "/home/ballo/terra/terraform/ECR"
   ecr_info         = var.ecr_info
 }
 
 module "RDS" {
-  source           = "/home/ballo/terra/RDS"
+  source           = "/home/ballo/terra/terraform/RDS"
   RDS_subnets_id   = module.Network.RDS_subnets_id
   sg_id            = module.SG.sg_id
   rds_info         = var.rds_info
